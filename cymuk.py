571ee7c0d434aad07e894edebc97e8203d4c1e6c
Đức Đào <duc.dao.431@gmail.com>
2023-01-01 16:56:47 +0700

    ✨ feat(script): add cymuk.py

---
+++ cymuk.py
@@ -0,0 +1,13 @@
+from modules import config_handler as ch
+from modules import keyboard_listener as kl
+from modules import mouse_continuous as mc
+from modules import mouse_jump as mj
+from modules import script_handler as sh
+
+
+def main():
+    ch.read_config_file()
+
+
+if __name__ == "__main__":
+    main()
