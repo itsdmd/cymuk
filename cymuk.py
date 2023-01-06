846cfd84813186e55cca57ddbaef5d895de779eb
Đức Đào <duc.dao.431@gmail.com>
2023-01-06 11:44:24 +0700

    ✨ feat(cymuk): use global props & add support for Monitor mode

--- cymuk.py
+++ cymuk.py
@@ -1,26 +1,36 @@
 from modules.cli_args import parser
-from modules.keyboard_listener import listener, set_verbose as kl_sv
-from modules.script_handler import parse_script, set_verbose as sh_sv
+from modules.keyboard_listener import listener
+from modules.monitor import main as monitor
+from modules.performer import centralize
+from modules.script_handler import parse_script
+import modules.global_properties as gp
 
 
 def main():
     args = vars(parser.parse_args())
 
-    if (args["script"] != None):
-        if (args["verbose"]):
-            kl_sv()
-            sh_sv()
-            print("Script mode")
+    if (args["verbose"]):
+        gp.set_verbose()
 
-        print(parse_script(args["script"]))
-
+    if (args["version"]):
+        print("cymuk v{}".format(gp.get_version()))
         return
 
+    if (args["script"] != None):
+        if gp.verbose:
+            print("Script mode")
+            print(parse_script(args["script"]))
+        return
+
+    elif (args["mouse_position"]):
+        if gp.verbose:
+            print("Monitor mode. Ctrl+C to exit.")
+        monitor()
+
     else:
-        if (args["verbose"]):
-            kl_sv()
-            sh_sv()
+        if gp.verbose:
             print("Capture mode")
+        centralize()
         listener.start()
         listener.join()
 
